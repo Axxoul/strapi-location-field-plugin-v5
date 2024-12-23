a4697a0748c4eea559cbbddf56763610952cfd7e
Emre Sanden <emresanden@gmail.com>
2024-12-23 10:50:58 +0100

    chore: update plugin version to 5.0.0 and refactor PluginIcon component

--- admin/src/components/PluginIcon/index.js
+++ admin/src/components/PluginIcon/index.js
@@ -1,23 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import { Icon } from "@strapi/design-system/Icon";
-import { Flex } from "@strapi/design-system/Flex";
 import { PinMap } from "@strapi/icons";
 
-const IconBox = styled(Flex)`
-    background-color: #f0f0ff;
-    border: 1px solid #d9d8ff;
-    svg > path {
-        fill: #4285f4;
-    }
+const IconBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 28px; /* Equivalent to 7 * 4px */
+  height: 24px; /* Equivalent to 6 * 4px */
+  border-radius: 4px; /* Use a specific radius value */
+  background-color: #f0f0ff;
+  border: 1px solid #d9d8ff;
+
+  svg > path {
+    fill: #4285f4;
+  }
 `;
 
 const MapPickerIcon = () => {
-    return (
-        <IconBox justifyContent="center" alignItems="center" width={7} height={6} hasRadius aria-hidden>
-            <Icon as={PinMap} />
-        </IconBox>
-    );
+  return (
+    <IconBox aria-hidden>
+      <Icon as={PinMap} />
+    </IconBox>
+  );
 };
 
 export default MapPickerIcon;
